f6c742b709fc5d6453d529241c33c8686cb5ee03
davidckim <davidckim320@gmail.com>
2015-06-16 01:40:26 -0700

    added features and animations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
 
+  $(".mar-image").hover(function() {
+      $(this).animate({
+        width: "29%"
+      }, 300 );
+    },
+    function() { 
+      $(this).animate({
+        width: "20%"
+      }, 200 );
+    }
+  )
 
 });
